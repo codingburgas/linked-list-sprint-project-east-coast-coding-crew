56c25cc817c18fe925fe540251f53d95a29db76a
AMKolev22 <amkolev22@codingburgas.bg>
2025-03-29 11:02:03 +0200

    Build script can be modified with changing variable names instead of hard-coded

---
+++ eccc/core/include/database.hpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "../../dependecies/websocketpp/client.hpp"
--- premake5.lua
+++ premake5.lua
@@ -1,27 +1,22 @@
-workspaceName = "East-Coast-Coding-Crew"
-workspace(workspaceName)
-
+workspaceName = "Eccc"
+workspace (workspaceName)
 architecture "x86_64"
-
 configurations { "Debug", "Release" }
-
 project "Core"
   local outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-  local workSpaceBuildName = "eccc"
+  local workSpaceBuildName = workspaceName
   local depFolderName = "dependencies"
-
   kind "ConsoleApp"
-  
   language "C++"
-  cppdialect "C++Latest"
-
+  cppdialect "C++23"
   location ("./" .. workSpaceBuildName .. "/%{prj.name}")
-
   targetdir ("./bin/" .. outputdir .. "/%{prj.name}")
   objdir ("./bin-int/" .. outputdir .. "/%{prj.name}")
-
-  files { "./" .. outputdir .. "/%{prj.name}/src/**.cpp", "./" .. outputdir .. "/%{prj.name}/include/**.h", "./" .. outputdir .. "/%{prj.name}/include/**.hpp" }
-
+  files {
+    "Eccc/Core/src/.cpp",
+    "Eccc/Core/include/.h",
+    "Eccc/Core/include/*.hpp"
+  }
   includedirs {
     "./" .. depFolderName .. "/libpq/include",
     "./" .. depFolderName .. "/openssl/include",
@@ -37,9 +32,23 @@ project "Core"
     "./" .. depFolderName .. "/websocketpp",
     "./" .. workSpaceBuildName .. "/%{prj.name}/include"
   }
-
-  filter "configurations:*"
-    libdirs { "./" .. depFolderName .. "/libpq/lib", "./" .. depFolderName .. "/openssl/lib" }
+  filter "configurations:"
+    libdirs { "./" .. depFolderName .. "/libpq/lib", "./" .. depFolderName .. "/openssl/lib",
+      "./" .. depFolderName .. "/fmt/src", 
+      "./" .. depFolderName .. "/ftxui/src" ,
+      "./" .. depFolderName .. "/libpq/include",
+      "./" .. depFolderName .. "/openssl/include",
+      "./" .. depFolderName .. "/cpr/curl/include",
+      "./" .. depFolderName .. "/cpr/include",
+      "./" .. depFolderName .. "/fmt/include",
+      "./" .. depFolderName .. "/ftxui/include",
+      "./" .. depFolderName .. "/wfrest/base",
+      "./" .. depFolderName .. "/wfrest/core",
+      "./" .. depFolderName .. "/wfrest/util",
+      "./" .. depFolderName .. "/nlohmann",
+      "./" .. depFolderName .. "/sqlite_orm",
+      "./" .. depFolderName .. "/websocketpp",
+    }
     links {
       "libpq",
       "libssl",
@@ -48,23 +57,54 @@ project "Core"
       "ftxui",
       "cpr"
     }
-
   systemversion "latest"
-
   filter "configurations:Debug"
     defines { "easteregg_DEBUG" }
     symbols "On"
-    libdirs { "./" .. depFolderName .. "/cpr/libs/Debug-libs", "./" .. depFolderName .. "/fmt/src", "./" .. depFolderName .. "/ftxui/src" }
-    links { 
+    libdirs { 
+        "./" .. depFolderName .. "/cpr/libs/Debug-libs", 
+        "./" .. depFolderName .. "/fmt/src", 
+        "./" .. depFolderName .. "/ftxui/src" ,
+        "./" .. depFolderName .. "/libpq/include",
+        "./" .. depFolderName .. "/openssl/include",
+        "./" .. depFolderName .. "/cpr/curl/include",
+        "./" .. depFolderName .. "/cpr/include",
+        "./" .. depFolderName .. "/fmt/include",
+        "./" .. depFolderName .. "/ftxui/include",
+        "./" .. depFolderName .. "/wfrest/base",
+        "./" .. depFolderName .. "/wfrest/core",
+        "./" .. depFolderName .. "/wfrest/util",
+        "./" .. depFolderName .. "/nlohmann",
+        "./" .. depFolderName .. "/sqlite_orm",
+        "./" .. depFolderName .. "/websocketpp",
+    
+        }
+    links {
       "libcurl-d_imp",
       "cpr"
     }
-  
+    
   filter "configurations:Release"
     defines { "easteregg_RELEASE" }
     optimize "On"
-    libdirs { "./" .. depFolderName .. "/cpr/libs/Release-libs", "./" .. depFolderName .. "/fmt/src", "./" .. depFolderName .. "/ftxui/src" }
-    links { 
+    libdirs { "./" .. depFolderName .. "/cpr/libs/Release-libs", 
+        "./" .. depFolderName .. "/fmt/src", 
+        "./" .. depFolderName .. "/ftxui/src",
+        "./" .. depFolderName .. "/ftxui/src" ,
+        "./" .. depFolderName .. "/libpq/include",
+        "./" .. depFolderName .. "/openssl/include",
+        "./" .. depFolderName .. "/cpr/curl/include",
+        "./" .. depFolderName .. "/cpr/include",
+        "./" .. depFolderName .. "/fmt/include",
+        "./" .. depFolderName .. "/ftxui/include",
+        "./" .. depFolderName .. "/wfrest/base",
+        "./" .. depFolderName .. "/wfrest/core",
+        "./" .. depFolderName .. "/wfrest/util",
+        "./" .. depFolderName .. "/nlohmann",
+        "./" .. depFolderName .. "/sqlite_orm",
+        "./" .. depFolderName .. "/websocketpp",
+    }
+    links {
       "libcurl_imp",
       "cpr"
     }
