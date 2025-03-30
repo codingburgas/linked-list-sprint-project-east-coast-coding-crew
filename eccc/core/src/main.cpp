f9ab4c654701da8d89364bd435d9182e2e46e8e7
AMKolev22 <amkolev22@codingburgas.bg>
2025-03-30 11:53:06 +0300

    Main uses clearer type aliasing

--- eccc/core/src/main.cpp
+++ eccc/core/src/main.cpp
@@ -1,21 +1,21 @@
 #include "database.hpp"
 #include <iostream>
 using Eccc::Core::Database;
-using ASYNC_NoReturn = std::future<std::expected<void, std::string>>;
 
-auto instance = Eccc::Core::Database::getInstance(); // this is not a smart pointer to avoid double converting in the next line from a normal to a smart pointer
+Database::ASYNC_SINGLETON_RETURN instance = Eccc::Core::Database::getInstance(); // this is not a smart pointer to avoid double converting in the next line from a normal to a smart pointer
+
 auto databaseHandle = std::make_shared<std::expected<Database*, std::string>>(instance.get());
-std::shared_ptr<ASYNC_NoReturn> generealFutureHandle;
-
+std::shared_ptr<Database::ASYNC_NoReturn> generealFutureHandle;
+ 
 int main() {
 
-	if (!databaseHandle || !databaseHandle->has_value()) {
+	if (!databaseHandle.get() || !databaseHandle->has_value()) {
 		std::cout << (databaseHandle->has_value() ? databaseHandle->error() : "error");
 		return 1;
 	}
 
 	Database* db = databaseHandle->value();
-	generealFutureHandle = std::make_shared<ASYNC_NoReturn>(db->async_connectToDb());
+	generealFutureHandle = std::make_shared<Database::ASYNC_NoReturn>(db->async_connectToDb());
 	auto res = generealFutureHandle.get()->get();
 
 	if (!res.has_value()) {
